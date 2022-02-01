e84992d4b993d176e80ff5d13aa01f6c63f6f7fe
evilnerdpy <markstepanov88@gmail.com>
2022-02-02 04:20:39 +0500

    starting up

--- app/src/main/java/com/example/biglibrary/MainActivity.java
+++ app/src/main/java/com/example/biglibrary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.biglibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            Fragment mainPageFragment = new MainPageFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainFragmentContainerView, mainPageFragment)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/biglibrary/MainPageFragment.java
@@ -0,0 +1,33 @@
+package com.example.biglibrary;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class MainPageFragment extends Fragment {
+
+    private Button AddBookButton;
+    private Button FindBookButton;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_page, container, false);
+
+
+    }
+}
